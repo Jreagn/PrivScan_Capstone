85194b727b9ba49748ef213c2b19dab60b7b3e3a
Jreagn <113216795+Jreagn@users.noreply.github.com>
2026-02-22 20:00:08 -0500

    Corrections for filesize and processing speed
    Updated file upload handling to use headers for filename and increased max content length.

--- listen.py
+++ listen.py
@@ -3,19 +3,30 @@ from pathlib import Path
 from waitress import serve
 
 app = Flask(__name__)
-app.config["MAX_CONTENT_LENGTH"] = 500 * 1024 * 1024
+app.config["MAX_CONTENT_LENGTH"] = 800 * 1024 * 1024 * 1024
+
 UPLOAD_DIR = Path("uploads")
 UPLOAD_DIR.mkdir(exist_ok=True)
 
 @app.route("/scan", methods=["POST"])
 def scan():
-    print("Request received")
-    if "file" not in request.files:
-        return "No file field", 400
-    f = request.files["file"]
-    f.save(UPLOAD_DIR / f.filename)
-    print(f"Saved: {f.filename}")
+    filename = request.headers.get("X-Filename")
+    if not filename:
+        return "Missing filename header", 400
+
+    dest = UPLOAD_DIR / filename
+    chunk_size = 256 * 1024
+
+    with open(dest, "wb") as f:
+        while True:
+            chunk = request.stream.read(chunk_size)
+            if not chunk:
+                break
+            f.write(chunk)
+
     return "File received", 200
 
+
 print("Starting server...")
-serve(app, host="0.0.0.0", port=65432)
+# serve(app, host="0.0.0.0", port=65432, channel_timeout=1000000, asyncore_loop_timeout=5, connection_limit=1000)
+app.run(host="0.0.0.0", port=65432)
